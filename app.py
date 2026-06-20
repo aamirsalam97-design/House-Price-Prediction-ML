5a61d7b413b9ae4ea2f3debc47744eceaa008f1f
MOHD AAMIR SALAM <aamirsalam97@gmail.com>
2026-06-20 15:24:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,4 @@
 import streamlit as st
-import pandas as pd
-from sklearn.linear_model import LinearRegression
 
-# Data
-df = pd.read_csv("house_data.csv")
-
-X = df[["Area"]]
-y = df["Price"]
-
-model = LinearRegression()
-model.fit(X, y)
-
-st.title("🏠 House Price Prediction")
-
-area = st.number_input("Enter House Area", min_value=50)
-
-if st.button("Predict Price"):
-    prediction = model.predict([[area]])
-    st.success(f"Predicted Price: ₹ {int(prediction[0]):,}")
+st.title("House Price Prediction")
+st.success("App is working successfully!")
