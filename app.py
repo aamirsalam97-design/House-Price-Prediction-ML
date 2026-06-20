55ded394c3d5384fe6d0bf88b8147b5c34330b7f
MOHD AAMIR SALAM <aamirsalam97@gmail.com>
2026-06-20 15:59:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 st.title("House Price Prediction")
-st.success("Pandas loaded successfully!")
+st.success("Sklearn loaded successfully!")
